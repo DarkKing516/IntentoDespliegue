05342a120cdbf355c4669d83da5d3f9a845cad2f
DarkKing516 <jhomai7020@gmail.com>
2024-07-21 16:45:03 -0500

    ahor xd

--- Erikas_HomeMade/settings.py
+++ Erikas_HomeMade/settings.py
@@ -161,6 +161,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # and renames the files with unique names for each version to support long-term caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
